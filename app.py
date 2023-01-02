c1754cc935b20671c093c411cf7748d84f5120a5
nassimbouziane <bouzianenassim17102002@gmail.com>
2023-01-03 00:41:04 +0100

    [scrollbar] added canva to add a scrollbar if the standing overflow

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 root = Tk()
 root.title('Football result app by Nassim Bouziane')
 root.geometry('500x500')
-scrollbar = Scrollbar(root,orient='vertical')
+
 
 
 year = datetime.datetime.today().year - 1
@@ -20,7 +20,7 @@ config = dotenv_values('.env')
 print(config['API_KEY'])
 
 frm = ttk.Frame(root, padding=0)
-frm.grid()
+frm.pack()
 
 # Define Leagues Tuple
 Leagues= ('Premier League','Liga','Ligue 1','Serie A','Liga Nos')
@@ -54,7 +54,21 @@ def callback(*arg):
    labelStanding['relief'] = 'solid'
    
          
-      
+canvas = Canvas(root)
+scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
+scrollable_frame = ttk.Frame(canvas)
+scrollable_frame.bind(
+    "<Configure>",
+    lambda e: canvas.configure(
+        scrollregion=canvas.bbox("all")
+    )
+)
+canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+canvas.configure(yscrollcommand=scrollbar.set,borderwidth=0,highlightthickness=0)
+canvas.pack(side="left", fill="both", expand=True)
+scrollbar.pack(side="right", fill="y")
+
+
    
    
 # Create a combobox widget
@@ -64,29 +78,26 @@ var.set('Premier League')
 varYears = StringVar()
 varYears.set(datetime.datetime.today().year - 1)
 
-cb= ttk.Combobox(frm, textvariable= var)
+cb= ttk.Combobox(scrollable_frame, textvariable= var)
 cb['values']= Leagues
 cb['state']= 'readonly'
 cb.pack(fill='x',padx= 5, pady=5)
 
-label = Label(frm, text='Result of '+var.get())
+label = Label(scrollable_frame, text='Result of '+var.get())
 label.pack()
 
-cbYear = ttk.Combobox(frm,textvariable=varYears)
+cbYear = ttk.Combobox(scrollable_frame,textvariable=varYears)
 cbYear['values'] = lst_Years
 cbYear['state'] = 'readonly'
 cbYear.pack(fill='x', padx='10',pady='5')
 
-labelStanding = Label(frm)
-labelStanding.pack( padx='50')
+labelStanding = Label(scrollable_frame)
+labelStanding.pack()
 
 
 # Set the tracing for the given variable
 var.trace('w', callback)
 varYears.trace('w', callback)
-
-
-
 
 
 
